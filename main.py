6b26ce772fbd252eb211afadd140f2cdd2ba2b0c
Guilherme M. <guilhermematos.ofc@gmail.com>
2025-09-04 16:39:49 -0300

    Update main.py
    Acrescentado a função da grid visual, ajustado a main para uma função e inicialização depois das funções. 
    próximos passo criar a grade matemática e fazer uma câmera

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
-
 #Blibiotecas
 import pygame
 
-#Variáveis Globais#
-#Configurações
-altura = 1280
-largura = 680
-tamanho_da_celula = 20
-colunas = altura//tamanho_da_celula
-linhas = largura//tamanho_da_celula
-#Cores
-fundo = (30,30,30) 
-cor_da_grid = (100,100,100) 
+#Funções - Temporiamente vou fazer no mesmo arquivo
 
+#Grid visual
+def grid_visual(tela):
+    
+    #Configurações da grid
+    altura = 1280
+    largura = 680
+    tamanho_da_celula = 5
+    colunas = altura//tamanho_da_celula
+    linhas = largura//tamanho_da_celula
+    
+    #Cores
+    fundo = (30,30,30) 
+    cor_da_grid = (100,100,100)
 
+    #Execução
+    tela.fill(fundo)
+    for x in range(0, altura, tamanho_da_celula):
+        pygame.draw.line(tela, cor_da_grid, (x, 0), (x, largura))
+    for y in range(0, largura, tamanho_da_celula):
+        pygame.draw.line(tela, cor_da_grid, (0, y), (altura, y))
+        
+#Ececução do programa
 def main():
+    
     #Start e Parâtros da Janela
     pygame.init()
-    tela = pygame.display.set_mode((altura,largura)) #Tamanho da Tela       
+
+    tela = pygame.display.set_mode((1280,680)) #Tamanho da Tela       
     pygame.display.set_caption("Vila I.A.")   #Nome Provisório
 
     #Loop de Execução Global
@@ -32,8 +45,13 @@ def main():
             if eventos.type == pygame.QUIT:
                 loop = False
                 
+        grid_visual(tela) #Mostrar a grid
+        pygame.display.flip()
+        
     pygame.quit()
 
+#Iniciar Programa
+main()
 
 
 
@@ -41,5 +59,6 @@ def main():
 
 
 
-Vila I.A.py
-Exibindo Vila I.A.py. 
+
+
+
