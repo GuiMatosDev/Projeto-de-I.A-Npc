f2c79d406b87fc68747d08ba955267fd8960bf66
Guilherme M. <guilhermematos.ofc@gmail.com>
2025-09-09 10:05:59 -0300

    Update main.py
    Redefinição de algumas variáveis, criação da função de grade matemática (mapa), reservada para futuras implementações de estruturas. definição das variáveis de camera e inicio da criação da função desenhar_mapa que vai mostrar na janela o que mexermos na grade matemática.

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
 #Blibiotecas
 import pygame
 
-#Funções - Temporiamente vou fazer no mesmo arquivo
+#Configurações da Janela
+altura_tela = 1280
+largura_tela = 680
+tamanho_da_celula = 20
 
-#Grid visual
-def grid_visual(tela):
-    
-    #Configurações da grid
-    altura = 1280
-    largura = 680
-    tamanho_da_celula = 5
-    colunas = altura//tamanho_da_celula
-    linhas = largura//tamanho_da_celula
-    
-    #Cores
-    fundo = (30,30,30) 
-    cor_da_grid = (100,100,100)
+#Configurações do Mapa
+mapa_coluna = 100
+mapa_linhas = 100
 
-    #Execução
-    tela.fill(fundo)
-    for x in range(0, altura, tamanho_da_celula):
-        pygame.draw.line(tela, cor_da_grid, (x, 0), (x, largura))
-    for y in range(0, largura, tamanho_da_celula):
-        pygame.draw.line(tela, cor_da_grid, (0, y), (altura, y))
+#Cores
+fundo = (30,30,30) 
+cor_da_grade = (100,100,100)
+cor_obstaculo = (180, 60, 60)
+cor_debug = (200, 200, 50)
+
+#Mapa
+def criar_mapa(colunas,linhas):
+    
+    grid = [[ 0 for x in range(colunas)] for y in range(linhas)]
+
+    #Futuras Criações de estruturas, reservar valores e alterar de zero para 1: Obstaculo
+    
+
+#Visual    
+def desenhar_mapa(tela, grid, cam_coluna, cam_linha)
+                        
         
-#Ececução do programa
+#Execução do programa
 def main():
     
-    #Start e Parâtros da Janela
+    #Start e Parâtros 
     pygame.init()
-
-    tela = pygame.display.set_mode((1280,680)) #Tamanho da Tela       
-    pygame.display.set_caption("Vila I.A.")   #Nome Provisório
+    pygame.display.set_caption("Path Neural")   #Nome Provisório
+    
+    tela = pygame.display.set_mode((1280,680)) #Tamanho da Tela        
+    
+    grid = criar_mapa(mapa_coluna, mapa_linha)
+    cam_coluna = 0
+    cam_linha = 0
 
     #Loop de Execução Global
     loop = True
@@ -44,14 +52,18 @@ def main():
             #Fechando o Aplicativo
             if eventos.type == pygame.QUIT:
                 loop = False
-                
-        grid_visual(tela) #Mostrar a grid
-        pygame.display.flip()
-        
+
+
+        desenhar_mapa(tela, grid, cam_coluna, cam,linha)
+        pygame.display.flip() 
+
     pygame.quit()
 
 #Iniciar Programa
 main()
+
+
+
 
 
 
